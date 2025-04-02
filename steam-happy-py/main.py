cf620c88ba4868af194f74d357ae6ed48f833b2a
jcward7 <jward3322@gmail.com>
2025-04-02 03:49:17 -0500

    catch up python rewrite to the same as the js version - Server command implemented that can now start the minecraft server

--- steam-happy-py/main.py
+++ steam-happy-py/main.py
@@ -1,30 +1,45 @@
 import discord
 import json
+import subprocess
+from discord import app_commands
 
 with open('config.json', 'r') as file:
     data = json.load(file)
 
 BOT_TOKEN = data['token']
-BOT_CLIENT = data['clientId']
-BOT_GUILD = data['guildId']
+MC_PATH = data['mcPath']
+BOT_CLIENT = discord.Object(id=data['clientId'])
+BOT_GUILD = discord.Object(id=data['guildId'])
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'------')
-        print(f'Logged on as {self.user}!')
-        print(f'------')
-    
-    async def on_message(self, message):
-        if message.author.id == client.user.id:
-            return
-        
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=BOT_GUILD)
+        await self.tree.sync(guild=BOT_GUILD)
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = MyClient(intents=intents)
 
+@client.event
+async def on_ready():
+    print(f'------')
+    print(f'Logged on as {client.user}!')
+    print(f'------')
+
+
+@client.tree.command(name = "hello", description = "Says hello to the user!")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f'{interaction.user.mention} Hi!')
+
+
+@client.tree.command(name = "server", description = "Starts the minecraft server.")
+async def server(interaction: discord.Interaction):
+    server = subprocess.Popen([MC_PATH])
+    await interaction.response.send_message(f'{interaction.user.mention} Server has now started!')
 
 client.run(BOT_TOKEN)
